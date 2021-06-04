3b6447640ea93247311326a8432d6b962c84e83f
Martin Allard <mar.allard@gmail.com>
2021-06-04 18:49:28 +0200

    Include mysqldump in the installed binaries

--- lib/build_pack/installer.rb
+++ lib/build_pack/installer.rb
@@ -61,7 +61,11 @@ module BuildPack
         # ```
         # binaries = Dir.glob("#{@mysql_binaries}/*")
         # ```
-        mysqldump_binary = Dir.glob("#{@mysql_binaries}/mysql")
+        mysql_binary = Dir.glob("#{@mysql_binaries}/mysql")
+        FileUtils.chmod("u=wrx", mysql_binary)
+        FileUtils.mv(mysql_binary, @bin_path)
+        
+        mysqldump_binary = Dir.glob("#{@mysql_binaries}/mysqldump")
         FileUtils.chmod("u=wrx", mysqldump_binary)
         FileUtils.mv(mysqldump_binary, @bin_path)
       end
